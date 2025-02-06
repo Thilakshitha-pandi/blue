e12223862c41e390b778e03b00c86a85ac89a3aa
Thilakshitha-pandi <thilakshitha0903@gmail.com>
2025-02-06 14:25:27 +0530

    Update VowelChecker.java

--- VowelChecker.java
+++ VowelChecker.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
 
 public class VowelChecker {
-public static void main(String[] args) {
-Scanner scanner = new Scanner(System.in);
-System.out.print("Enter a character: ");
-char input = scanner.next().toLowerCase().charAt(0); 
-if (input == 'a' || input == 'e' || input == 'i' || input == 'o' || input == 'u') {
-System.out.println(input + " is a vowel.");
-} 
-else {
-System.out.println(word + " is not a vowel.");
-}
-scanner.close();
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char input = scanner.next().toLowerCase().charAt(0); 
+        switch (input) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(input + " is a vowel.");
+                break;
+            default:
+                System.out.println(input + " is not a vowel.");
+                break;
+        }
+
+        scanner.close();
+    }
 }
