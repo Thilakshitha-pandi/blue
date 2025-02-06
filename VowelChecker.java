593c339eeb6fed638f922586814cc2e415a4fddc
Thilakshitha-pandi <thilakshitha0903@gmail.com>
2025-02-06 14:09:08 +0530

    Update VowelChecker.java

--- VowelChecker.java
+++ VowelChecker.java
@@ -4,12 +4,13 @@ public class VowelChecker {
 public static void main(String[] args) {
 Scanner scanner = new Scanner(System.in);
 System.out.print("Enter a character: ");
-char input = scanner.next().toLowerCase().charAt(0);  // Convert input to lowercase to make the check case-insensitive
+char word = scanner.next();
+char input = word.toLowerCase().charAt(0);  // Convert input to lowercase to make the check case-insensitive
 if (input == 'a' || input == 'e' || input == 'i' || input == 'o' || input == 'u') {
 System.out.println(input + " is a vowel.");
 } 
 else {
-System.out.println(input + " is not a vowel.");
+System.out.println(word + " is not a vowel.");
 }
 scanner.close();
 }
