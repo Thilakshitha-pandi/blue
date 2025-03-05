3d61dd72870d13cf936ed11c4103ae451e87a4dd
Thilakshitha-pandi <thilakshitha0903@gmail.com>
2025-03-05 21:30:22 +0530

    Create SwapNo.java

---
+++ Day 1 /SwapNo.java
@@ -0,0 +1,18 @@
+public class SwapNo {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+
+        
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a + ", b = " + b);
+
+        
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        
+        System.out.println("After swapping:");
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}
