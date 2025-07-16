2b4ca4cf39d6b295687eaa5c449c180496fc47e9
Thilakshitha-pandi <thilakshitha0903@gmail.com>
2025-07-16 09:08:14 +0530

    Update and rename task1.js to Home_task.js

--- DAY 1 JS/task1.js
+++
@@ -1,6 +0,0 @@
-let num1 = prompt("Enter the first number:");
-let num2 = prompt("Enter the second number:");
-num1 = Number(num1);
-num2 = Number(num2);
-console.log("Sum is: " + (num1 + num2));
-document.write("Sum is: " + (num1 + num2));
